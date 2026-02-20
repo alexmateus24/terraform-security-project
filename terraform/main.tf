2debd7b14bbf9a836e817ebcbe6c929fd82be945
alexmateus24 <alexmateus247@live.com>
2026-02-20 18:42:34 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,14 +11,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-
-  # CI / policy evaluation only
-  skip_credentials_validation = true
-  skip_requesting_account_id  = true
-  skip_metadata_api_check     = true
-}
 
 # ISSUE 1: S3 bucket without encryption
 resource "aws_s3_bucket" "data_bucket" {
