1ebc5657e15ef8f6d22acebd29e5814e13c140d6
alexmateus24 <alexmateus247@live.com>
2026-02-20 18:43:06 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,6 @@
 # QuantumTrade Infrastructure - CURRENT STATE (INSECURE)
 # This file contains intentional security misconfigurations
 
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-
 # ISSUE 1: S3 bucket without encryption
 resource "aws_s3_bucket" "data_bucket" {
   bucket = "quantumtrade-transaction-data"
